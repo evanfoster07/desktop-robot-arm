ecc24013ebc62b92be16e3f2cc49d7f520434c25
Evan Foster <evanfoster2007@gmail.com>
2026-08-31 08:38:28 -0400

    re-train creeper-2 run on new close-up images

--- src/raspberry-pi/src/vision/train_yolo.py
+++ src/raspberry-pi/src/vision/train_yolo.py
@@ -3,15 +3,17 @@ from pathlib import Path
 
 
 def main():
-    model = YOLO("yolo26n.pt")
+    model = YOLO("src/vision/runs/detect/creeper-2/weights/best.pt")
 
     output_dir = Path(__file__).parent / "runs" / "detect"
 
     model.train(
         data="dataset/data.yaml",
-        epochs=30,
+        epochs=20,
         imgsz=640,
         freeze=10,
+        batch=8,
+        workers=2,
         project=output_dir,
         name="creeper"
     )
